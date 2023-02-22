6a87d2ab01948a246bdafac02395a730c4bd162a
lTimej <lTimej@126.com>
2023-02-23 00:23:29 +0800

    初始化项目

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Time_Cloud_Disk
 this is a lightweight cloud disk
+
+goctl api new core
+
+
---
+++ core/internal/handler/corehandler.go
@@ -0,0 +1,28 @@
+package handler
+
+import (
+	"net/http"
+
+	"github.com/zeromicro/go-zero/rest/httpx"
+	"liujun/Time_Cloud_Disk/core/internal/logic"
+	"liujun/Time_Cloud_Disk/core/internal/svc"
+	"liujun/Time_Cloud_Disk/core/internal/types"
+)
+
+func CoreHandler(svcCtx *svc.ServiceContext) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var req types.Request
+		if err := httpx.Parse(r, &req); err != nil {
+			httpx.ErrorCtx(r.Context(), w, err)
+			return
+		}
+
+		l := logic.NewCoreLogic(r.Context(), svcCtx)
+		resp, err := l.Core(&req)
+		if err != nil {
+			httpx.ErrorCtx(r.Context(), w, err)
+		} else {
+			httpx.OkJsonCtx(r.Context(), w, resp)
+		}
+	}
+}
